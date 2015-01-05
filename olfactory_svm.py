ac2634c0c189c82e6ec193a0b7f4edc585d492c4
Myke Walton <walton.myke@gmail.com>
2015-01-05 01:43:00 -0800

    removed redundant files

--- olfactory_svm.py
+++ olfactory_svm.py
@@ -5,139 +5,136 @@ Created on Fri Nov 21 16:41:42 2014
 @author: michaelwalton
 """
 
+#this is set up as a multiclass problem, could maybe modify to a multilabel
+#problem and then treat the p(label) as a prediciton / reproduction of the
+#odorant concentration, this would be really cool if that representation works
+
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
+import scipy as sp
+import evolvedmachines as em
 
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import RandomizedSearchCV
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
-expType = 3
-preprocess = True
-tuneHyperparams = False
+dataFolder = "data/Otrain_4Otest/" #folders: Otrain_4Otest, OBGtrain_4OBGtest, Otrain_4OBGtest
+target_names = ['odorant 0', 'odorant 1', 'odorant 2', 'odorant 3']
 
-###############################################################################
-# Pick a dataset
-# As the project grows, this should be replaced by a line arg.
-# to set a containing folder then run on the data in that dir
-if (expType == 0):
-    train_conc_file = "data/Otrain_4Otest/train_c.csv"
-    train_actv_file = "data/Otrain_4Otest/train_a.csv"
-    test_conc_file = "data/Otrain_4Otest/test_c.csv"
-    test_actv_file = "data/Otrain_4Otest/test_a.csv"
-elif (expType == 1):
-    train_conc_file = "data/OBGtrain_4OBGtest/train_c.csv"
-    train_actv_file = "data/OBGtrain_4OBGtest/train_a.csv"
-    test_conc_file = "data/OBGtrain_4OBGtest/test_c.csv"
-    test_actv_file = "data/OBGtrain_4OBGtest/test_a.csv"
-elif (expType == 2):
-    train_conc_file = "data/Otrain_4OBGtest/train_c.csv"
-    train_actv_file = "data/Otrain_4OBGtest/train_a.csv"
-    test_conc_file = "data/Otrain_4OBGtest/test_c.csv"
-    test_actv_file = "data/Otrain_4OBGtest/test_a.csv"
-else:
-    train_conc_file = "data/Otrain_4OBGtest/train_c.csv"
-    train_actv_file = "data/Otrain_4OBGtest/train_a_rs.csv"
-    test_conc_file = "data/Otrain_4OBGtest/test_c.csv"
-    test_actv_file = "data/Otrain_4OBGtest/test_a_rs.csv"
+addNoise = False
+doRsa = True
+standardize = True
+parameterEstimation = 'random' #options: none, exhaustive, random, fixed_range
+rand_iter = 10 #number of samples in the parameter space to sample in random estimation mode
 
 ###############################################################################
 #load data
-reader = csv.reader(open(train_conc_file,"rb"), delimiter=",")
+reader = csv.reader(open(dataFolder + "train_c.csv","rb"), delimiter=",")
 x = list(reader)
 train_c = np.array(x).astype('float')
 
-reader = csv.reader(open(train_actv_file,"rb"), delimiter=",")
+reader = csv.reader(open(dataFolder + "train_a.csv","rb"), delimiter=",")
 x = list(reader)
 train_a = np.array(x).astype('float')
 
-reader = csv.reader(open(test_conc_file,"rb"), delimiter=",")
+reader = csv.reader(open(dataFolder + "test_c.csv","rb"), delimiter=",")
 x = list(reader)
 test_c = np.array(x).astype('float')
 
-reader = csv.reader(open(test_actv_file,"rb"), delimiter=",")
+reader = csv.reader(open(dataFolder + "test_a.csv","rb"), delimiter=",")
 x = list(reader)
 test_a = np.array(x).astype('float')
 
 ###############################################################################
-# Clean up the data
-#get max(concentration) foreach t (target is max odorant index)
-train_target = np.zeros([train_c.shape[0]], dtype=float)
+# Convert the concentration labels to classes
+train_target = np.argmax(train_c[:,1:5], axis=1)
+test_target = np.argmax(test_c[:,1:5], axis=1)
 
-for i in range(train_c.shape[0]):
-    maxC = 0
-    for j in range(train_c.shape[1]):
-        if (train_c[i][j] > maxC and j > 0):
-            maxC = train_c[i][j]
-            train_target[i] = j - 1
-            
-test_target = np.zeros([test_c.shape[0]], dtype=float)
-
-for i in range(test_c.shape[0]):
-    maxC = 0
-    for j in range(test_c.shape[1]):
-        if (test_c[i][j] > maxC and j > 0):
-            maxC = test_c[i][j]
-            test_target[i] = j - 1
+###############################################################################
+# Add noise to training and test sets
+if (addNoise):
+    random_state = np.random.RandomState(0)
+    
+    n_samples, n_features = train_a.shape
+    train_a = np.c_[train_a, random_state.randn(n_samples, n_features)]
+    
+    n_samples, n_features = test_a.shape
+    test_a = np.c_[test_a, random_state.randn(n_samples, n_features)]
 
 ###############################################################################
 # Data Pre-processing
-# Improves classification on 
-if (preprocess):
+if (doRsa):
+    rsa = em.RSA(latencyScale=100, sigmoidRate=False, normalizeSpikes=True,
+                 maxLatency=1000, maxSpikes=20)
+                 
+    train_a = rsa.countNspikes(train_a)
+    test_a = rsa.countNspikes(test_a)
+
+if (standardize):
     scaler = StandardScaler()
-    train_a = scaler.fit_transform(train_a, train_target)
-
-###############################################################################
-# Train and test the SVM
-# train svm classifier
-
-if (tuneHyperparams):
-    C_range = 10.0 ** np.arange(-1, 1)
-    gamma_range = 10.0 ** np.arange(-1, 1)
-    param_grid = dict(gamma=gamma_range, C=C_range)
-    cv = StratifiedKFold(y=train_target, n_folds=3)
-    grid = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv)
-    grid.fit(train_a, train_target)
-    print("Best Classifier: ", grid.best_estimator_)
-    bestClf = grid.best_estimator_
-    bestPred = bestClf.predict(test_a)
+    train_a = scaler.fit_transform(train_a)
+    test_a = scaler.transform(test_a)
     
-    score_dict = grid.grid_scores_
-    scores = [z[1] for z in score_dict]
-    scores = np.array(scores).reshape(len(C_range), len(gamma_range))
-    pl.figure(10)
-    pl.imshow(scores, interpolation='nearest', cmap=pl.cm.spectral)
-    pl.xlabel('gamma')
-    pl.ylabel('C')
-    pl.colorbar()    
-    pl.show()
+###############################################################################
+# Build multiple classifiers based on some parameter space (random, grid, etc.)
+# assess each using stratified k-folds of test data, select the best estimator
 
-clf = svm.SVC()
-print("Default classifier: ", clf)
-clf.fit(train_a, train_target)
+if (parameterEstimation == 'none'):
+    clf = svm.SVC()
+    clf.fit(train_a, train_target)
+else:
+    #configure stratified k-fold cross validation               
+    cv = StratifiedKFold(y=train_target, n_folds=3, shuffle=True)
+    
+    #set the parameter grid
+    if (parameterEstimation == 'exhaustive'):
+        #do exhaustive gridsearch within some range
+        kernel_range = ['rbf', 'linear']
+        gamma_range = np.arange(start=1e-3, stop=1e-1, step=1e-3)
+        C_range = np.arange(1,1000)
+        param_grid = dict(kernel=kernel_range, gamma=gamma_range, C=C_range)
+        grid = GridSearchCV(svm.SVC(C=1), param_grid=param_grid, cv=cv)
+                        
+    elif(parameterEstimation == 'random'):
+        #randomly sample values within some distribution of params
+        kernel_range = ['rbf', 'linear']
+        class_weight_range = ['auto', None]
+        gamma_range =  sp.stats.expon(scale=.1)
+        C_range = sp.stats.expon(scale=100)
+        param_dist = dict(kernel=kernel_range, gamma=gamma_range, C=C_range,
+                          class_weight=class_weight_range)
+        
+        grid = RandomizedSearchCV(svm.SVC(), param_distributions=param_dist,
+                                  cv=cv, n_iter=rand_iter)
+        
+    elif(parameterEstimation == 'fixed_range'):
+        #exhaustive search on an explictly defined dictionary of params
+        param_grid = [{'kernel': ['rbf'], 'gamma': [1e-1, 1e-5],
+                        'C': [1, 10, 100, 1000]},
+                        {'kernel': ['linear'],
+                        'C': [1, 10, 100, 1000]}]
+        grid = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv)
 
-# run the prediction
+    #fit and validate until we find the best estimator within the range
+    grid.fit(train_a, train_target)
+    
+    #show the resulting estimators
+    for params, mean_score, scores in grid.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() / 2, params))
+
+    print("Best Estimator: %s" % grid.best_estimator_)
+    clf = grid.best_estimator_
+    
+###############################################################################
+#RUN PREDICTION
 pred = clf.predict(test_a)
-
-# test classification
-correctPredictions = 0
-bestClfCorrect = 0
-for i in range(pred.shape[0]):
-    if(pred[i] == test_target[i]):
-        correctPredictions += 1.0
-    if (tuneHyperparams and bestPred[i] == test_target[i]):
-        bestClfCorrect += 1.0
-
-defaultClfPerformance = correctPredictions / test_target.shape[0]
-optoClfPerformance = bestClfCorrect / test_target.shape[0]
-
-print("Default H-params", defaultClfPerformance)
-print("Optomized H-params", optoClfPerformance)
-print("Difference", optoClfPerformance - defaultClfPerformance)
 
 ###############################################################################
 #PLOT DATA
@@ -146,6 +143,8 @@ print("Difference", optoClfPerformance - defaultClfPerformance)
 pl.figure(1)
 plt.plot(train_c)
 plt.title('Training (Odorant Concentration)')
+plt.yscale('log')
+plt.ylim(1e-4, 1)
 plt.ylabel('Concentration')
 plt.xlabel('Time')
 plt.show()
@@ -153,6 +152,8 @@ plt.show()
 pl.figure(2)
 plt.plot(test_c)
 plt.title('Testing (Odorant Concentration)')
+plt.yscale('log')
+plt.ylim(1e-4, 1)
 plt.ylabel('Concentration')
 plt.xlabel('Time')
 plt.show()
@@ -165,38 +166,6 @@ plt.ylabel('Activation')
 plt.xlabel('Time')
 plt.show()
 
-#show confusion matrix
-cm = confusion_matrix(test_target, pred)
-pl.figure(7)
-plt.matshow(cm)
-plt.colorbar()
-plt.ylabel('Target label')
-plt.xlabel('Predicted label')
-plt.show()
-
-"""
-pl.figure(3, figsize=(6,6))
-plt.imshow(np.transpose(train_a)[:, 25:150])
-#plt.colorbar()
-plt.title('Training (Sensor Pattern)')
-plt.ylabel('Activation')
-plt.xlabel('Time')
-plt.show()
-
-pl.figure(2)
-plt.plot(train_target)
-plt.title('Training (Target Odorant)')
-plt.ylabel('Odorant Index')
-plt.xlabel('Time')
-plt.show()
-
-pl.figure(5)
-plt.plot(test_target)
-plt.title('Testing (Target Odorant)')
-plt.ylabel('Odorant Index')
-plt.xlabel('Time')
-plt.show()
-
 pl.figure(6)
 plt.imshow(np.transpose(test_a))
 #plt.colorbar()
@@ -204,13 +173,19 @@ plt.title('Testing (Sensor Pattern)')
 plt.ylabel('Activation')
 plt.xlabel('Time')
 plt.show()
-"""
 
-if (tuneHyperparams):
-    cm = confusion_matrix(test_target, bestPred)
-    pl.figure(8)
-    plt.matshow(cm)
-    plt.colorbar()
-    plt.ylabel('Target label')
-    plt.xlabel('Predicted label')
-    plt.show()
+#show confusion matrix
+cm = confusion_matrix(test_target, pred)
+pl.figure(7)
+plt.matshow(cm)
+plt.colorbar()
+plt.title('SVC')
+plt.ylabel('Target label')
+plt.xlabel('Predicted label')
+plt.show()
+
+print("\n")
+print(classification_report(test_target, pred, target_names=target_names))
+print("Accuracy Score: %s\n" % accuracy_score(test_target, pred))
+print("Classifier Settings: %s" % clf)
+#print("AP", average_precision_score(test_target, pred))
