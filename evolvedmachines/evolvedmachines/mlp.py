ac839509190eb7e67f56bb4bae09db1515f94f92
Myke Walton <walton.myke@gmail.com>
2015-01-05 16:10:30 -0800

    its beginning to look like pybrain may be too limited in terms of performance, compatibility w/ numpy / matplotlib and general documentaiton.  This was a good exercise for getting everything organized and putting together a simple net, but it is looking like I may need to use a more sophistocated tool (eg: theano / pylearn2)

--- evolvedmachines/evolvedmachines/mlp.py
+++ evolvedmachines/evolvedmachines/mlp.py
@@ -4,8 +4,7 @@ from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer
 
-from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
-from scipy import arange, meshgrid, where
+#from scipy import arange, meshgrid
 
 from data import olfactoryDataset
 
@@ -17,21 +16,11 @@ class NN:
         self.data = olfactoryDataset()
         self.data.load(folder)
         
-    def buildNetwork(self, hiddenLayer=5):
+    def buildNetwork(self, hiddenLayer=5, monentum=0.1, weightdecay=0.01):
         self.fnn = buildNetwork( self.data.train_dataset.indim, hiddenLayer, self.data.train_dataset.outdim, outclass=SoftmaxLayer )
-        self.trainer = BackpropTrainer( self.fnn, dataset=self.data.train_dataset, momentum=0.1, verbose=True, weightdecay=0.01)
+        self.trainer = BackpropTrainer( self.fnn, dataset=self.data.train_dataset, momentum=monentum, verbose=True, weightdecay=weightdecay)
 
     def runTraining(self, n=20):
-        ticks = arange(-3.,6.,0.2)
-        X, Y = meshgrid(ticks, ticks)
-        # need column vectors in dataset, not arrays
-        griddata = ClassificationDataSet(2,1, nb_classes=4)
-        for i in xrange(X.size):
-            griddata.addSample([X.ravel()[i],Y.ravel()[i]], [0])
-        griddata._convertToOneOfMany()  # this is still needed to make the fnn feel comfy
-        
-        #self.trainer.trainEpochs(n)
-        
         for i in range(n):
             self.trainer.trainEpochs(1)
             trnresult = percentError( self.trainer.testOnClassData(),
@@ -42,22 +31,4 @@ class NN:
             print "epoch: %4d" % self.trainer.totalepochs, \
                   "  train error: %5.2f%%" % trnresult, \
                   "  test error: %5.2f%%" % tstresult
-            out = self.fnn.activateOnDataset(griddata)
-            out = out.argmax(axis=1)  # the highest output activation gives the class
-            out = out.reshape(X.shape)
-            
-            figure(1)
-            ioff()  # interactive graphics off
-            clf()   # clear the plot
-            hold(True) # overplot on
-            for c in [0,1,2,3]:
-                here, _ = where(self.data.test_dataset['class']==c)
-                plot(self.data.test_dataset['input'][here,0],self.data.test_dataset['input'][here,1],'o')
-            if out.max()!=out.min():  # safety check against flat field
-                contourf(X, Y, out)   # plot the contour
-            ion()   # interactive graphics on
-            draw()  # update the plot
-            
-        ioff()
-        show()
 
--- olfactory_mlp.py
+++ olfactory_mlp.py
@@ -1,69 +1,8 @@
-from pybrain.datasets            import ClassificationDataSet
-from pybrain.utilities           import percentError
-from pybrain.tools.shortcuts     import buildNetwork
-from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules   import SoftmaxLayer
+from evolvedmachines.mlp import NN
 
-from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
-from scipy import diag, arange, meshgrid, where
-from numpy.random import multivariate_normal
+dataFolder = "data/Otrain_4Otest/" #folders: Otrain_4Otest, OBGtrain_4OBGtest, Otrain_4OBGtest
 
-means = [(-1,0),(2,4),(3,1),(0,2)]
-cov = [diag([1,1]), diag([0.5,1.2]), diag([1.5,0.7]), diag([1.2,0.1])]
-alldata = ClassificationDataSet(2, 1, nb_classes=4)
-for n in xrange(400):
-    for klass in range(4):
-        input = multivariate_normal(means[klass],cov[klass])
-        alldata.addSample(input, [klass])
-        
-tstdata, trndata = alldata.splitWithProportion( 0.25 )
-
-
-trndata._convertToOneOfMany( )
-tstdata._convertToOneOfMany( )
-
-print "Number of training patterns: ", len(trndata)
-print "Input and output dimensions: ", trndata.indim, trndata.outdim
-print "First sample (input, target, class):"
-print trndata['input'][0], trndata['target'][0], trndata['class'][0]
-
-fnn = buildNetwork( trndata.indim, 8, trndata.outdim, outclass=SoftmaxLayer )
-
-trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
-
-ticks = arange(-3.,6.,0.2)
-X, Y = meshgrid(ticks, ticks)
-# need column vectors in dataset, not arrays
-griddata = ClassificationDataSet(2,1, nb_classes=4)
-for i in xrange(X.size):
-    griddata.addSample([X.ravel()[i],Y.ravel()[i]], [0])
-griddata._convertToOneOfMany()  # this is still needed to make the fnn feel comfy
-
-for i in range(20):
-    trainer.trainEpochs( 1 )
-    trnresult = percentError( trainer.testOnClassData(),
-                              trndata['class'] )
-    tstresult = percentError( trainer.testOnClassData(
-           dataset=tstdata ), tstdata['class'] )
-
-    print "epoch: %4d" % trainer.totalepochs, \
-          "  train error: %5.2f%%" % trnresult, \
-          "  test error: %5.2f%%" % tstresult
-    out = fnn.activateOnDataset(griddata)
-    out = out.argmax(axis=1)  # the highest output activation gives the class
-    out = out.reshape(X.shape)
-    
-    figure(1)
-    ioff()  # interactive graphics off
-    clf()   # clear the plot
-    hold(True) # overplot on
-    for c in [0,1,2,3]:
-        here, _ = where(tstdata['class']==c)
-        plot(tstdata['input'][here,0],tstdata['input'][here,1],'o')
-    if out.max()!=out.min():  # safety check against flat field
-        contourf(X, Y, out)   # plot the contour
-    ion()   # interactive graphics on
-    draw()  # update the plot
-    
-ioff()
-show()
+network = NN()
+network.loadData(dataFolder)
+network.buildNetwork(hiddenLayer=50, monentum=0.2, weightdecay=0.01)
+network.runTraining(n=20)
