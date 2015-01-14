7fdc4ac2ba7bb4a281726283d740e335e3632899
Myke Walton <walton.myke@gmail.com>
2015-01-14 00:20:50 -0800

    model fit optimization

--- optimize.sh
+++ optimize.sh
@@ -6,5 +6,5 @@ yTrain=train_c.csv
 xTest=test_a.csv
 yTest=test_c.csv
 
-#python rbm.py --xTrain $path/$xTrain --yTrain $path/$yTrain --xTest $path/$xTest --yTest $path/$yTest --optimize 1
+python rbm.py --xTrain $path/$xTrain --yTrain $path/$yTrain --xTest $path/$xTest --yTest $path/$yTest --optimize 1
 python svm.py --xTrain $path/$xTrain --yTrain $path/$yTrain --xTest $path/$xTest --yTest $path/$yTest --optimize 1
