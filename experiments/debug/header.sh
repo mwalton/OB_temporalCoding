1496f855f092a5ffc8c27b931f1977239b4d7e79
Myke Walton <walton.myke@gmail.com>
2015-01-14 16:38:03 -0800

    gitignore .csv datasets, moved runscripts to experiments folder

---
+++ experiments/debug/header.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+path=data/debug/Otrain_4Otest
+
+xTrain=$path/train_a.csv
+yTrain=$path/train_c.csv
+xTest=$path/test_a.csv
+yTest=$path/test_c.csv
+emNSpath=$path/tstepAccuracy.csv
--- experiments/debug/runtest.sh
+++ experiments/debug/runtest.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-emNSpath=tstepAccuracy.csv
-
-path=data/Otrain_4Otest
-xTrain=$path/train_a.csv
-yTrain=$path/train_c.csv
-xTest=$path/test_a.csv
-yTest=$path/test_c.csv
+source experiments/debug/header.sh
 
 python svm.py --xTrain $xTrain --yTrain $yTrain --xTest $xTest --yTest $yTest --optimize 0 --visualize 0
 python rbm.py --xTrain $xTrain --yTrain $yTrain --xTest $xTest --yTest $yTest --optimize 2 --visualize 0
