0e0b6c6649a7955dc40154a89f3fcb012cd41899
Myke Walton <walton.myke@gmail.com>
2015-01-27 00:57:00 -0800

    evo pca

--- irisPCA.py
+++ irisPCA.py
@@ -1,40 +1,52 @@
-print(__doc__)
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+import argparse
+from sklearn.preprocessing import StandardScaler
 
 from sklearn import decomposition
 from sklearn import datasets
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file", required = True,
+    help = "path to the evolution run file")
+args = vars(ap.parse_args())
 
 np.random.seed(5)
 
 centers = [[1, 1], [-1, -1], [1, -1]]
 iris = datasets.load_iris()
-X = iris.data
+
+raw = np.genfromtxt(args["file"], delimiter=',', dtype=float)
+
+X = raw[:,7:12]
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 y = iris.target
 
 fig = plt.figure(1, figsize=(8, 6))
 plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+ax = Axes3D(fig, elev=48, azim=134)
 
 plt.cla()
 pca = decomposition.PCA(n_components=3)
 pca.fit(X)
-X = pca.transform(X)
+X3d = pca.transform(X)
 
 for name, label in [('Setosa', 0), ('Versicolour', 1), ('Virginica', 2)]:
+    """
     ax.text3D(X[y == label, 0].mean(),
               X[y == label, 1].mean() + 1.5,
               X[y == label, 2].mean(), name,
               horizontalalignment='center',
               bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+              """
 # Reorder the labels to have colors matching the cluster results
 y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral)
+ax.scatter(X3d[:, 0], X3d[:, 1], X3d[:, 2], c=raw[:,30], cmap=plt.cm.spectral)
 
+"""
 x_surf = [X[:, 0].min(), X[:, 0].max(),
           X[:, 0].min(), X[:, 0].max()]
 y_surf = [X[:, 0].max(), X[:, 0].max(),
@@ -49,5 +61,5 @@ v1 /= v1[-1]
 ax.w_xaxis.set_ticklabels([])
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
-
+"""
 plt.show()
