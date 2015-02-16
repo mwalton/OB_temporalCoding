446df03cc2fe55e920dd75bd6011169b383f61a2
Myke Walton <walton.myke@gmail.com>
2015-02-16 14:30:21 -0800

    SVM multi-color plots

--- python/svrTest.py
+++ python/svrTest.py
@@ -14,9 +14,10 @@ def loadData(XPath, yPath):
 def standardize(featureVector):
     scaler = StandardScaler()
     return scaler.fit_transform(featureVector)
+
 """
-xtrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG1/0.01test/sensorActivation.csv"
-ytrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG1/0.01test/concentration.csv"
+xtrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG1/0.01train/sensorActivation.csv"
+ytrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG1/0.01train/concentration.csv"
 xtestpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG2/0.19test/sensorActivation.csv"
 ytestpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/BGtest/BG2/0.19test/concentration.csv"
 """
@@ -25,6 +26,7 @@ xtrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/
 ytrainpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/kaggle/paul_medC_BG2/train/concentration.csv"
 xtestpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/kaggle/paul_highC_BG1/test/sensorActivation.csv"
 ytestpath="/Users/michaelwalton/Dropbox/Evolved Machines 2014/Machine Learning/datasets/kaggle/paul_highC_BG1/test/concentration.csv"
+
 
 (Xtrain, ytrain) = loadData(xtrainpath, ytrainpath)
 (X,y) = loadData(xtestpath, ytestpath)
@@ -37,8 +39,8 @@ y = y[:,1:]
 y[y<1e-3]=1e-3
 y=np.log10(y)
 
-X = standardize(X)
-Xtrain = standardize(Xtrain)
+#X = standardize(X)
+#asdfdfXtrain = standardize(Xtrain)
 
 ###############################################################################
 # Fit regression model
@@ -85,10 +87,10 @@ poly_rmse.append(sqrt(mean_squared_error(y[:,3], y_poly[3])))
 fig = plt.figure(figsize=(20,10))
 
 ax1 = fig.add_subplot(241)
-ax1.plot(y_rbf[0], c='g', label='RBF Fit')
+ax1.plot(y_rbf[0], c='r', label='RBF Fit')
 #ax1.plot(y_lin[0], c='r', label='Linear Fit')
 #ax1.plot(y_poly[0], c='b', label='Poly Fit')
-ax1.plot(y[:,0], c='y', label='Target')
+ax1.plot(y[:,0], c='grey', label='Target')
 ax1.set_xlabel('Time')
 ax1.set_ylabel('log[c]')
 ax1.set_title('SVR O1')
@@ -98,37 +100,37 @@ ax2 = fig.add_subplot(242)
 ax2.plot(y_rbf[1], c='g', label='RBF Fit')
 #ax2.plot(y_lin[1], c='r', label='Linear Fit')
 #ax2.plot(y_poly[1], c='b', label='Poly Fit')
-ax2.plot(y[:,1], c='y', label='Target')
+ax2.plot(y[:,1], c='grey', label='Target')
 ax2.set_xlabel('Time')
-ax2.set_ylabel('log[c]')
+#ax2.set_ylabel('log[c]')
 ax2.set_title('SVR O2')
 ax2.legend()
 
 ax3 = fig.add_subplot(243)
-ax3.plot(y_rbf[2], c='g', label='RBF Fit')
+ax3.plot(y_rbf[2], c='b', label='RBF Fit')
 #ax3.plot(y_lin[2], c='r', label='Linear Fit')
 #ax3.plot(y_poly[2], c='b', label='Poly Fit')
-ax3.plot(y[:,2], c='y', label='Target')
+ax3.plot(y[:,2], c='grey', label='Target')
 ax3.set_xlabel('Time')
-ax3.set_ylabel('log[c]')
+#ax3.set_ylabel('log[c]')
 ax3.set_title('SVR O3')
 ax3.legend()
 
 ax4 = fig.add_subplot(244)
-ax4.plot(y_rbf[3], c='g', label='RBF Fit')
+ax4.plot(y_rbf[3], c='y', label='RBF Fit')
 #ax4.plot(y_lin[3], c='r', label='Linear Fit')
 #ax4.plot(y_poly[3], c='b', label='Poly Fit')
-ax4.plot(y[:,3], c='y', label='Target')
+ax4.plot(y[:,3], c='grey', label='Target')
 ax4.set_xlabel('Time')
-ax4.set_ylabel('log[c]')
+#ax4.set_ylabel('log[c]')
 ax4.set_title('SVR O4')
 ax4.legend()
 
 ax5 = fig.add_subplot(245)
-ax5.scatter(y[:,0], y_rbf[0], c='g', label=('RBF RMSE=%0.2f' % rbf_rmse[0]))
+ax5.scatter(y[:,0], y_rbf[0], c='r', label=('RBF RMSE=%0.2f' % rbf_rmse[0]))
 #ax5.scatter(y[:,0], y_lin[0], c='r', label=('Linear RMSE=%0.2f' % lin_rmse[0]))
 #ax5.scatter(y[:,0], y_poly[0], c='b', label=('Polynomial RMSE=%0.2f' % poly_rmse[0]))
-ax5.plot(y[:,0],y[:,0],c='y')
+ax5.plot(y[:,0],y[:,0],c='grey')
 ax5.set_xlim(np.min(y[:,0]), np.max(y[:,0]))
 ax5.set_xlabel('Prediction')
 ax5.set_ylabel('Actual')
@@ -138,30 +140,30 @@ ax6 = fig.add_subplot(246)
 ax6.scatter(y[:,1], y_rbf[1], c='g', label=('RBF RMSE=%0.2f' % rbf_rmse[1]))
 #ax6.scatter(y[:,1], y_lin[1], c='r', label=('Linear RMSE=%0.2f' % lin_rmse[1]))
 #ax6.scatter(y[:,1], y_poly[1], c='b', label=('Polynomial RMSE=%0.2f' % poly_rmse[1]))
-ax6.plot(y[:,1],y[:,1],c='y')
+ax6.plot(y[:,1],y[:,1],c='grey')
 ax6.set_xlim(np.min(y[:,1]), np.max(y[:,1]))
 ax6.set_xlabel('Prediction')
-ax6.set_ylabel('Actual')
+#ax6.set_ylabel('Actual')
 ax6.legend()
 
 ax7 = fig.add_subplot(247)
-ax7.scatter(y[:,2], y_rbf[2], c='g', label=('RBF RMSE=%0.2f' % rbf_rmse[2]))
+ax7.scatter(y[:,2], y_rbf[2], c='b', label=('RBF RMSE=%0.2f' % rbf_rmse[2]))
 #ax7.scatter(y[:,2], y_lin[2], c='r', label=('Linear RMSE=%0.2f' % lin_rmse[2]))
 #ax7.scatter(y[:,2], y_poly[2], c='b', label=('Polynomial RMSE=%0.2f' % poly_rmse[2]))
-ax7.plot(y[:,2],y[:,2],c='y')
+ax7.plot(y[:,2],y[:,2],c='grey')
 ax7.set_xlim(np.min(y[:,2]), np.max(y[:,2]))
 ax7.set_xlabel('Prediction')
-ax7.set_ylabel('Actual')
+#ax7.set_ylabel('Actual')
 ax7.legend()
 
 ax8 = fig.add_subplot(248)
-ax8.scatter(y[:,3], y_rbf[3], c='g', label=('RBF RMSE=%0.2f' % rbf_rmse[3]))
+ax8.scatter(y[:,3], y_rbf[3], c='y', label=('RBF RMSE=%0.2f' % rbf_rmse[3]))
 #ax8.scatter(y[:,3], y_lin[3], c='r', label=('Linear RMSE=%0.2f' % lin_rmse[3]))
 #ax8.scatter(y[:,3], y_poly[3], c='b', label=('Polynomial RMSE=%0.2f' % poly_rmse[3]))
-ax8.plot(y[:,3],y[:,3],c='y')
+ax8.plot(y[:,3],y[:,3],c='grey')
 ax8.set_xlim(np.min(y[:,3]), np.max(y[:,3]))
 ax8.set_xlabel('Prediction')
-ax8.set_ylabel('Actual')
+#ax8.set_ylabel('Actual')
 ax8.legend()
 
 plt.show()
